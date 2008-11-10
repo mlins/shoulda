68ac7da4222e92c2af41fca5c725c585ce15cce0
Matt Lins <mattlins@gmail.com>
2008-11-10 09:11:52 -0600

    added dependent option to should_belong_to

--- test/unit/dog_test.rb
+++ test/unit/dog_test.rb
@@ -2,6 +2,6 @@ require File.dirname(__FILE__) + '/../test_helper'
 
 class DogTest < Test::Unit::TestCase
   should_belong_to :user
-  should_belong_to :address
+  should_belong_to :address, :dependent => :destroy
   should_have_and_belong_to_many :fleas
 end
