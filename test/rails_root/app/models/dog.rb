9fe6da2f35b468ed322a7d33337f69e680fef255
Matt Lins <mattlins@gmail.com>
2008-11-10 14:07:07 -0600

    should_require_attributes now handles arrays (has_many and has_and_belongs_many)

--- test/rails_root/app/models/dog.rb
+++ test/rails_root/app/models/dog.rb
@@ -1,5 +1,7 @@
 class Dog < ActiveRecord::Base
   belongs_to :user, :foreign_key => :owner_id
   belongs_to :address
+  has_many :treats
   has_and_belongs_to_many :fleas, :join_table => :fleas
+  validates_presence_of :treats, :fleas
 end
